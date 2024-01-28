0d89cc191efa479f1b05bff033bda3f17675892c
langeblake <114412977+langeblake@users.noreply.github.com>
2024-01-28 21:06:01 +0900

    removed pnpm; changed fetch date for latast data

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,6 @@ import type { Config } from "tailwindcss"
 
 const config = {
   darkMode: ["class"],
-
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
@@ -73,9 +72,7 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
-      
     },
-    
   },
   plugins: [require("tailwindcss-animate")],
 } satisfies Config
