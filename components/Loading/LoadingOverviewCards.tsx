09310a6c5eed6e38644aea487e602b9587c981a5
langeblake <114412977+langeblake@users.noreply.github.com>
2024-05-18 17:16:35 +0900

    Loading styles

--- components/Loading/LoadingOverviewCards.tsx
+++ components/Loading/LoadingOverviewCards.tsx
@@ -4,12 +4,12 @@ const SkeletonCard = () => {
   return (
     <div className="animate-pulse w-full h-full shadow-md rounded-lg p-4 border dark:border-zinc-700 dark:bg-zinc-900">
       <div className="space-y-3">
-        <div className="h-5 bg-gray-700 rounded"></div>
-        <div className="h-5 bg-gray-700 rounded"></div>
-        <div className="h-5 bg-gray-700 rounded"></div>
-        <div className="h-5 bg-gray-700 rounded"></div>
-        <div className="h-5 bg-gray-700 rounded"></div>
-        <div className="h-5 bg-gray-700 rounded"></div>
+        <div className="h-5 bg-zinc-300 dark:bg-zinc-700 rounded"></div>
+        <div className="h-5 bg-zinc-300 dark:bg-zinc-700 rounded"></div>
+        <div className="h-5 bg-zinc-300 dark:bg-zinc-700 rounded"></div>
+        <div className="h-5 bg-zinc-300 dark:bg-zinc-700 rounded"></div>
+        <div className="h-5 bg-zinc-300 dark:bg-zinc-700 rounded"></div>
+        <div className="h-5 bg-zinc-300 dark:bg-zinc-700 rounded"></div>
       </div>
     </div>
   );
--- components/Loading/LoadingTickerTable.tsx
+++ components/Loading/LoadingTickerTable.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
-const LoadingSkeleton = ({ columnsCount = 8, rowsCount = 10 }) => {
-  const renderSkeletonRow = (key) => (
-    <TableRow key={key} className="border-b border-zinc-600">
-      {Array.from({ length: columnsCount }).map((_, index) => (
-        <TableCell key={index} className="h-12">
-          <div className="bg-gray-700 animate-pulse h-4 w-full rounded" />
-        </TableCell>
-      ))}
-    </TableRow>
-  );
+const LoadingSkeleton = ({ columnsCount = 1, rowsCount = 10 }) => {
+    const renderSkeletonRow = (key) => (
+        <TableRow key={key} className="border-b border-zinc-600">
+          {Array.from({ length: columnsCount }).map((_, index) => (
+            <TableCell key={index} className="h-12">
+              <div className="bg-zinc-300 dark:bg-zinc-700 animate-pulse h-4 w-full rounded" />
+            </TableCell>
+          ))}
+        </TableRow>
+      );
 
   return (
     <div className="w-full overflow-x-auto">
       <Table className="border-b border-zinc-600">
         <TableHeader className="dark:bg-zinc-900 bg-zinc-200 h-14">
           <TableRow className="border-zinc-600">
-            {Array.from({ length: columnsCount }).map((_, index) => (
+          {Array.from({ length: columnsCount }).map((_, index) => (
               <TableHead key={index} className="text-right w-40">
-                <div className="bg-gray-700 animate-pulse h-4 w-full rounded" />
+                <div className="bg-zinc-300 dark:bg-zinc-700 animate-pulse h-4 w-full rounded" />
               </TableHead>
             ))}
           </TableRow>
